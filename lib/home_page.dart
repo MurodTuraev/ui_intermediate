4850c57dda0037c33f7d8e278ac36ee9c712a953
macshop <tml.murod@mail.ru>
2023-04-11 16:14:32 +0500

    drawer

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,58 +14,36 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  void _iosDialog(){
-    showCupertinoDialog(context: context,
-        builder: (context) => CupertinoAlertDialog(
-          title: Text('LogOut'),
-          content: Text('Rostdan ham chiqmoqchimisiz?'),
-          actions: [
-            CupertinoDialogAction(
-              child: Text('YOQ'),
-              onPressed: () => Navigator.of(context).pop(),
-            ),
-            CupertinoDialogAction(child: Text('HA'),
-            onPressed: () => Navigator.of(context).pop(),)
-          ],
-        )
-    );
-  }
-
-  void _androidDialog(){
-    showDialog(context: context, builder: (BuildContext){
-      return AlertDialog(
-        title: Text('LogOut'),
-        content: Text('Rostdan ham chiqmoqchimisiz?'),
-        actions: [
-          TextButton(onPressed: (){
-            Navigator.of(context).pop();
-          },
-              child: Text('YOQ'),
-          ),
-          TextButton(onPressed: (){
-            Navigator.of(context).pop();
-          },
-            child: Text('HA'),
-          )
-        ],
-      );
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: TextButton(
-          child: Text('HOME PAGE'),
-          onPressed: (){
-            if(Platform.isAndroid){
-              _androidDialog();
-            }else if(Platform.isIOS){
-              _iosDialog();
-            }
-          },
-        )
+
+      ),
+      appBar: AppBar(
+        title: Text('INS'),
+      ),
+      drawer: Drawer(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              height: 200,
+              color: Colors.black12,
+              child: Column(
+
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Murod Turaev', style: TextStyle(color: Colors.blue),),
+                  Text('murod.turaev@gmail.com', style: TextStyle(color: Colors.blue),),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
