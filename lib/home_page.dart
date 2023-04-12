f40efc30cb33adfea4acfb435fdd99bc302412af
macshop <tml.murod@mail.ru>
2023-04-12 15:15:53 +0500

    task1

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -16,35 +16,106 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(length: 2, child: Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: Text('INS Demo'),
-        bottom: TabBar(
-          tabs: [
-            Tab(icon: Icon(Icons.home),text: 'Home',),
-            Tab(icon: Icon(Icons.school),text: 'School',),
+        title: Text('INS demo'),
+      ),
+      drawer: Drawer(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.all(20),
+                color: Colors.lightBlue,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(50),
+                        child: Image(
+                          image: AssetImage('assets/images/images.jpeg'), height: 100, width: 100,
+                        ),
+                      ),
+                      padding: EdgeInsets.only(bottom: 10),
+                    ),
+                    Text('Murod Turaev', style: TextStyle(fontSize: 20, color: Colors.white),),
+                    Text('abc@gmail.com', style: TextStyle(fontSize: 20, color: Colors.white),),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    TextButton(
+                      onPressed: (){},
+                      child: Row(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            child: Icon(
+                              Icons.home
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(5),
+                            child: Text('Home', style: TextStyle(fontSize: 18),),
+                          )
+                        ],
+                      ),
+
+                    ),
+                    TextButton(
+                      onPressed: (){},
+                      child: Row(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            child: Icon(
+                                Icons.person
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(5),
+                            child: Text('Profile', style: TextStyle(fontSize: 18),),
+                          )
+                        ],
+                      ),
+
+                    ),
+                    TextButton(
+                      onPressed: (){},
+                      child: Row(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            child: Icon(
+                                Icons.people
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(5),
+                            child: Text('About us', style: TextStyle(fontSize: 18),),
+                          )
+                        ],
+                      ),
+
+                    ),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
-      body: TabBarView(
-        children: [
-          Container(
-            color: Colors.green,
-            child: Center(
-              child: Text('Birinchi sahifa', style: TextStyle(color: Colors.white, fontSize: 24),),
-            ),
-          ),
-          Container(
-            color: Colors.green,
-            child: Center(
-              child: Text('Ikkinchi sahifa', style: TextStyle(color: Colors.redAccent, fontSize: 24),),
-            ),
-          )
-        ],
-      ),
-    ),
     );
-
   }
 
 }
