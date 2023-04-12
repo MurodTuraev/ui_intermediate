4a99c27a4d8899127f466941f8216cda3299d728
macshop <tml.murod@mail.ru>
2023-04-12 12:32:38 +0500

    BottomNavigationBar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,12 +13,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  PageController _pageController = new PageController();
+  int _selectedPage = 0;
 
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _pageController = PageController();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
+        controller: _pageController,
+        onPageChanged: (int index){
+          setState(() {
+            _selectedPage = index;
+          });
+        },
         children: [
           Container(
             color: Colors.grey,
@@ -43,7 +58,31 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('INS'),
       ),
-
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home', backgroundColor: Colors.purpleAccent),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Message', backgroundColor: Colors.purpleAccent),
+          BottomNavigationBarItem(icon: Icon(Icons.ondemand_video_outlined), label: 'Video', backgroundColor: Colors.purpleAccent),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications), label: 'Notification', backgroundColor: Colors.purpleAccent),
+        ],
+        currentIndex: _selectedPage,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.black54,
+        backgroundColor: Colors.black,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        onTap: (int index){
+          setState(() {
+            _selectedPage = index;
+            _pageController.animateToPage(index, duration: Duration(microseconds: 200), curve: Curves.easeIn);
+          });
+        },
+      ),
     );
+  }
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<PageController>('_pageController', _pageController));
   }
 }
