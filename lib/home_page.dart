7917e62758d61a86a3956564b93edb3be58eb675
macshop <tml.murod@mail.ru>
2023-04-14 10:31:07 +0500

    SpeedDial

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,15 +14,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final _formKey = GlobalKey<FormState>();
-  String _email='', _password='';
-  void _doSignIn(){
-    // print(_formKey.currentState.mounted);
-    if(_formKey.currentState!.validate()){
-      _formKey.currentState!.save();
-      print('Welcome');
-    }
-  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,38 +22,42 @@ class _HomePageState extends State<HomePage> {
         title: Text('INS demo'),
       ),
       backgroundColor: Colors.white,
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Email'
-                ),
-                validator: (input) => !input.toString().contains('@') ? 'Notogri kiriting' : null,
-                onSaved: (input) => _email=input.toString(),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    labelText: 'Password'
-                ),
-                validator: (input) => input.toString().length<6 ? 'parol uzunligi 6 dan kop bolishi kerak' : null,
-                onSaved: (input) => _password=input.toString(),
-              ),
-            ),
-            TextButton(
-              onPressed: (){
-                _doSignIn();
-              },
-              child: Text('Sign in', style: TextStyle(color: Colors.redAccent, fontSize: 18),) ,
-            )
-          ],
-        ),
+      body: Center(
+        child: Text('Home Page', style: TextStyle(color: Colors.green, fontSize: 32),),
+      ),
+      floatingActionButton: SpeedDial(
+        animatedIcon: AnimatedIcons.menu_close,
+        animatedIconTheme: IconThemeData(size: 22),
+        overlayColor: Colors.black,
+        overlayOpacity: 0.5,
+        onOpen: ()=> print('Open'),
+        onClose: ()=> print('Close'),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        shape: CircleBorder(),
+        children: [
+          SpeedDialChild(
+            child: Icon(Icons.accessibility),
+            backgroundColor: Colors.redAccent,
+            label: 'Birinchi',
+            labelStyle: TextStyle(fontSize: 20),
+            onTap: ()=> print('Birinchi')
+          ),
+          SpeedDialChild(
+              child: Icon(Icons.podcasts),
+              backgroundColor: Colors.green,
+              label: 'Ikkinchi',
+              labelStyle: TextStyle(fontSize: 20),
+              onTap: ()=> print('Ikkinchi')
+          ),
+          SpeedDialChild(
+              child: Icon(Icons.face),
+              backgroundColor: Colors.grey,
+              label: 'Uchinchi',
+              labelStyle: TextStyle(fontSize: 20),
+              onTap: ()=> print('Uchinchi')
+          )
+        ],
       ),
     );
   }
